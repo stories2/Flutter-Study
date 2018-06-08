c93b229511a66c1193b70564d5a231c207ac05e7
Kim HyunWoo <stories2@naver.com>
2018-06-08 14:26:15 +0900

    stateful widgets create layout

--- stateful_widgets/stateful_widgets/lib/main.dart
+++ stateful_widgets/stateful_widgets/lib/main.dart
@@ -3,9 +3,49 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      home: new Text(
-        "Hello!"
-      ),
+      home: new AwesomeButton(),
     )
   );
+}
+
+class AwesomeButton extends StatefulWidget {
+
+  @override
+  State createState() {
+    return new AwesomeButtonState();
+  }
+}
+
+class AwesomeButtonState extends State<AwesomeButton> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+            "Stateful widgets"
+        ),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                "Hello stateful"
+              ),
+              new RaisedButton(
+                  onPressed: null,
+                child: new Text(
+                  "Press me!"
+                ),
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
