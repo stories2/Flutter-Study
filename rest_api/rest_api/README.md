b062a797b2c7706acca0fc1b3adeff6313ad175f
Kim HyunWoo <stories2@naver.com>
2018-06-24 19:01:08 +0900

    get json data using http

---
+++ rest_api/rest_api/README.md
@@ -0,0 +1,8 @@
+# rest_api
+
+Example of REST API
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ rest_api/rest_api/lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as HttpManager;
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: new HomePage(),
+    )
+  );
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+
+  Future<String> GetData() async {
+    HttpManager.Response response = await HttpManager.get(
+      Uri.encodeFull("http://jsonplaceholder.typicode.com/posts"),
+      headers: {
+        "key": "asdf",
+        "Accept": "application/json"
+      }
+    );
+
+    print(response.body);
+
+    List responseList = JSON.decode(response.body);
+
+    print(responseList);
+
+    print("print specific title: " + responseList[1]["title"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.redAccent,
+        title: new Text(
+          "REST API"
+        ),
+      ),
+
+      body: new Center(
+        child: new RaisedButton(
+            onPressed: GetData,
+            child: new Text(
+              "Get data"
+            ),
+        ),
+      ),
+    );
+  }
+}
